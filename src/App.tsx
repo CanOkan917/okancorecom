53663f8f31b4f4137e3b0befdb39de747e7e5249
CanOkan <canokan@okancore.com>
2026-01-12 22:36:47 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-black flex items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-white text-6xl font-bold mb-4">
+                    Hello World
+                </h1>
+                <p className="text-gray-400 text-xl">
+                    okancore.com
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default App
